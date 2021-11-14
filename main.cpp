e46a74bcb468e817f17161d214f4ae2a162ca169
Zakaria6976 <91836213+Zakaria6976@users.noreply.github.com>
2021-11-14 16:23:50 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+    int n=0, i=0, l=1, k=0;
+    cin >> n;
+    cout << 1 <<endl;
+
+    while (k<=n) {
+
+        k=i+l;
+        if(k<=n){
+            cout << k <<endl;
+        }
+        i=l;
+        l=k;
+    }
+    return 0;
 }
